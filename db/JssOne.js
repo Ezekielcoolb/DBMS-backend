fe76ec4f216a3943ffa374267335a4de0a9ba733
Opaleye Boluwatife <ezekielcoolb@gmail.com>
2024-02-03 09:40:31 -0800

    m

--- db/JssOne.js
+++ db/JssOne.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const jssOneResultSchema = new mongoose.Schema({
-  results: [[String]] // Array of arrays of mixed types
+  results: [] // Array of arrays of mixed types
 });
 
 const JssOneResultModel = mongoose.model('jssOneResult', jssOneResultSchema);
